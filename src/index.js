b7a1ac2ff078293e0784e22cc0e93c51b4ccba62
Kristyna Vysoka <kristynavy@gmail.com>
2022-04-30 12:43:02 +0200

    order x cancel btn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 import './style.css';
 
 console.log('funguju!');
+
+const navBurger = document.querySelector('#nav-btn');
+const navElms = document.querySelector('nav');
+
+navBurger.addEventListener('click', () =>
+  navElms.classList.toggle('nav-closed'),
+);
+
+const navHide = document.querySelectorAll('nav a');
+navHide.forEach((item) =>
+  item.addEventListener('click', () => {
+    navElms.classList.add('nav-closed');
+  }),
+);
+
+const orderElm = document.querySelector('.order-btn');
+const cupElm = document.querySelector('.drink__cup');
+
+let ordered = false;
+orderElm.addEventListener('click', () => {
+  if (!ordered) {
+    orderElm.textContent = 'Zrušit';
+    cupElm.classList.add('drink__cup--selected');
+    ordered = true;
+  } else {
+    orderElm.textContent = 'Objednat';
+    cupElm.classList.remove('drink__cup--selected');
+    ordered = false;
+  }
+});
